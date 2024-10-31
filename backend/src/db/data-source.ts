eedc77353ecd3a18e7411080a281396665b7b018
QuyPham <quypham07102001@gmail.com>
2024-10-31 09:21:20 +0700

    add dockerfile

--- backend/src/db/data-source.ts
+++ backend/src/db/data-source.ts
@@ -1,12 +1,15 @@
 import { DataSourceOptions, DataSource } from 'typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 export const dataSourceOptions: DataSourceOptions = {
   type: 'mysql',
-  host: 'localhost',
-  port: 3309,
-  username: 'quypham',
-  password: 'Abc123456',
-  database: 'backend-db',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT, 10),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   entities: ['dist/**/*.entity.js'],
   migrations: ['dist/db/migrations/*.js'],
   synchronize: false,
