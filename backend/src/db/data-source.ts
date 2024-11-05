8ffb4ca03323d2a7cc50732f34a13ce352d90ce4
QuyPham <quypham07102001@gmail.com>
2024-11-05 11:13:21 +0700

    update file db

--- backend/src/db/data-source.ts
+++ backend/src/db/data-source.ts
@@ -5,10 +5,10 @@ dotenv.config();
 
 export const dataSourceOptions: DataSourceOptions = {
   type: 'mysql',
-  host: process.env.DB_HOST || 'localhost',
+  host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT) || 3306,
   username: process.env.DB_USERNAME,
-  password: process.env.Abc123456,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
   entities: ['dist/**/*.entity.js'],
   migrations: ['dist/db/migrations/*.js'],
