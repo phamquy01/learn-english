ebfc9a3c852855db3f282e75e475c20dac5978e0
QuyPham <quypham07102001@gmail.com>
2024-11-05 09:08:45 +0700

    deploy vercel

--- backend/src/db/data-source.ts
+++ backend/src/db/data-source.ts
@@ -5,11 +5,11 @@ dotenv.config();
 
 export const dataSourceOptions: DataSourceOptions = {
   type: 'mysql',
-  host: 'localhost',
-  port: 3309,
-  username: 'quypham',
-  password: 'Abc123456',
-  database: 'backend-db',
+  host: process.env.DB_HOST || 'localhost',
+  port: parseInt(process.env.DB_PORT) || 3306,
+  username: process.env.DB_USERNAME,
+  password: process.env.Abc123456,
+  database: process.env.DB_DATABASE,
   entities: ['dist/**/*.entity.js'],
   migrations: ['dist/db/migrations/*.js'],
   synchronize: false,
