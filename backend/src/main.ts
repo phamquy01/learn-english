f9c3aaa9497b46a3f227e28ef885ad69d8c7c395
QuyPham <quypham07102001@gmail.com>
2024-11-06 14:42:33 +0700

    fix cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,10 @@ import cors from 'cors';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  app.use(cors());
+  app.enableCors({
+    origin: ['learn-english-six-rose.vercel.app'],
+    credentials: true,
+  });
   app.use(cookieParser());
 
   app.setGlobalPrefix('api/v1', { exclude: [''] });
