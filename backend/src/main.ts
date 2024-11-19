839ab62dedc96b1ea88817c01ce58710a59518c8
QuyPham <quypham07102001@gmail.com>
2024-11-19 22:11:46 +0700

    deploy netlify

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -9,7 +9,10 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   app.use(cors());
   app.enableCors({
-    origin: ['learn-english-six-rho.vercel.app'],
+    origin: [
+      'learn-english-six-rho.vercel.app',
+      'golden-piroshki-b63f3e.netlify.app',
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
